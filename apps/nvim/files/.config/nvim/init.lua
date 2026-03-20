29fcbb8ae5922d2d0502d5512c5a1987801f0e48
TJ <you@example.com>
2026-03-20 13:10:04 -0400

    feat: switch to catppuccin frappe across nvim, tmux, ghostty

--- apps/nvim/files/.config/nvim/init.lua
+++ apps/nvim/files/.config/nvim/init.lua
@@ -7,9 +7,12 @@ require("config.autocmds")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
-		"git", "clone", "--filter=blob:none",
+		"git",
+		"clone",
+		"--filter=blob:none",
 		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", lazypath,
+		"--branch=stable",
+		lazypath,
 	})
 end
 vim.opt.rtp:prepend(lazypath)
@@ -17,6 +20,5 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	spec = { { import = "plugins" } },
 	defaults = { lazy = true, version = false },
-	install = { colorscheme = { "catppuccin-macchiato" } },
 	checker = { enabled = true },
 })
--- apps/nvim/files/.config/nvim/lua/plugins/ui/colorthemes/catppuccin.lua
+++ apps/nvim/files/.config/nvim/lua/plugins/ui/colorthemes/catppuccin.lua
@@ -3,37 +3,57 @@ return {
 	name = "catppuccin",
 	lazy = false,
 	priority = 1000,
-	opts = {
-		flavour = "macchiato", -- matches your tmux theme
-		transparent_background = false,
-		term_colors = true,
-		integrations = {
-			cmp = true,
-			gitsigns = true,
-			treesitter = true,
-			noice = true,
-			notify = true,
-			mini = { enabled = true },
-			lsp_trouble = false,
-			mason = true,
-			which_key = true,
-			snacks = true,
-			native_lsp = {
-				enabled = true,
-				virtual_text = {
-					errors      = { "italic" },
-					hints       = { "italic" },
-					warnings    = { "italic" },
-					information = { "italic" },
-				},
-				underlines = {
-					errors      = { "underline" },
-					hints       = { "underline" },
-					warnings    = { "underline" },
-					information = { "underline" },
-				},
-				inlay_hints = { background = true },
+	config = function()
+		require("catppuccin").setup({
+			flavour = "frappe",
+			transparent_background = false,
+			term_colors = true,
+			dim_inactive = {
+				enabled = false,
 			},
-		},
-	},
+			styles = {
+				comments = { "italic" },
+				conditionals = {},
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+			},
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				treesitter = true,
+				noice = true,
+				notify = true,
+				mini = { enabled = true },
+				lsp_trouble = false,
+				mason = true,
+				which_key = true,
+				snacks = true,
+				telescope = { enabled = false },
+				native_lsp = {
+					enabled = true,
+					virtual_text = {
+						errors = { "italic" },
+						hints = { "italic" },
+						warnings = { "italic" },
+						information = { "italic" },
+					},
+					underlines = {
+						errors = { "underline" },
+						hints = { "underline" },
+						warnings = { "underline" },
+						information = { "underline" },
+					},
+					inlay_hints = { background = true },
+				},
+			},
+		})
+		vim.cmd.colorscheme("catppuccin-frappe")
+	end,
 }
