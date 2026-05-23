dee098b3d81ccf501cf95431c7c33732d6c18043
TJ <you@example.com>
2026-05-22 23:08:30 -0400

    updates

--- apps/nvim/files/.config/nvim/lua/plugins/coding/init.lua
+++ apps/nvim/files/.config/nvim/lua/plugins/coding/init.lua
@@ -4,7 +4,6 @@ return {
 	{ import = "plugins.coding.mini-pairs" },
 	{ import = "plugins.coding.todo-comments" },
 	{ import = "plugins.coding.dotnet-hot-reload" },
-	{ import = "plugins.coding.ts-comments" },
 	{ import = "plugins.coding.claude" },
 	{ import = "plugins.coding.copilot-chat" },
 }
